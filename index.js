14ae8f7ac126bdeb1b02af2192fc27226dd5ab1c
Joseph Frampton <josephjamesframpton@gmail.com>
2021-03-20 15:35:08 -0600

    init
    JJF

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+// http server usually created automatically by express when just using "app"
+// we need access to the http server to pass into socket.io so we are explicitly creating the http server
+// the express app object is then passed into the http server function
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = 3001;
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket)=>{
+  console.log('a user connected');
+  socket.on('disconnect', ()=>{
+    console.log('a user disconnected');
+  })
+  socket.on('chat message', (msg)=>{
+    console.log(msg);
+    io.emit('chat message', msg);
+  })
+})
+
+http.listen(PORT, '0.0.0.0', ()=>{
+  console.log('listening on port ' + PORT);
+})
