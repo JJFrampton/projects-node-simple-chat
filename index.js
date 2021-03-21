9de6e207015e21d7d0ef29704c32902074b64409
Joseph Frampton <josephjamesframpton@gmail.com>
2021-03-20 18:20:22 -0600

    default to port 80
    JJF

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('express')();
 // the express app object is then passed into the http server function
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/index.html');
